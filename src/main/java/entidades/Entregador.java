ca0aabe3238ec814e3c42dca6d0f54f126aac74f
Viegas13 <mateusviegassilva10@gmail.com>
2024-04-15 18:45:57 -0300

    Atributos de entregador

--- src/main/java/entidades/Entregador.java
+++ src/main/java/entidades/Entregador.java
@@ -3,24 +3,53 @@ package entidades;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Entregador extends Pessoa{
+public class Entregador extends Pessoa {
 
-
-    private String veiculo;
+    private String tipoVeiculo;
+    private String marcaVeiculo;
+    private String placaVeiculo;
+    private long numeroCNH;
     private ArrayList<Entrega> entregas;
-    
-    public Entregador(String veiculo, ArrayList<Entrega> entregas, Date data) {
+
+    public Entregador(String tipoVeiculo, String marcaVeiculo, String placaVeiculo, long numeroCNH, ArrayList<Entrega> entregas, Date data) {
         super("a", "56567", "senha", "email", 5656685, data);
-        this.veiculo = veiculo;
+        this.tipoVeiculo = tipoVeiculo;
+        this.marcaVeiculo = marcaVeiculo;
+        this.placaVeiculo = placaVeiculo;
+        this.numeroCNH = numeroCNH;
         this.entregas = entregas;
     }
 
-    public String getVeiculo() {
-        return veiculo;
+    public String getTipoVeiculo() {
+        return tipoVeiculo;
     }
 
-    public void setVeiculo(String veiculo) {
-        this.veiculo = veiculo;
+    public void setTipoVeiculo(String tipoVeiculo) {
+        this.tipoVeiculo = tipoVeiculo;
+    }
+
+    public String getMarcaVeiculo() {
+        return marcaVeiculo;
+    }
+
+    public void setMarcaVeiculo(String marcaVeiculo) {
+        this.marcaVeiculo = marcaVeiculo;
+    }
+
+    public String getPlacaVeiculo() {
+        return placaVeiculo;
+    }
+
+    public void setPlacaVeiculo(String placaVeiculo) {
+        this.placaVeiculo = placaVeiculo;
+    }
+
+    public long getNumeroCNH() {
+        return numeroCNH;
+    }
+
+    public void setNumeroCNH(long numeroCNH) {
+        this.numeroCNH = numeroCNH;
     }
 
     public ArrayList<Entrega> getEntregas() {
@@ -30,5 +59,5 @@ public class Entregador extends Pessoa{
     public void setEntregas(ArrayList<Entrega> entregas) {
         this.entregas = entregas;
     }
-    
+
 }
