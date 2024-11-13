800b29581949f0582c131263c003726997fc59ba
unknown <rafa.igor06@gmail.com>
2024-11-13 14:31:54 -0300

    Tela de cadastro e campo senha

--- src/view-rapidex/interfaces/lib/View/perfil_fornecedor.dart
+++ src/view-rapidex/interfaces/lib/View/perfil_fornecedor.dart
@@ -24,103 +24,116 @@ class perfil_fornecedorScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          width: 300,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                'Dados fornecedor',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 16),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Nome',
-                  hintText: 'nome',
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 202, 200, 200),
-                ),
-                enabled: false,
-              ),
-              const SizedBox(height: 8),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'CNPJ',
-                  hintText: '1179891234',
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 202, 200, 200),
-                ),
-                enabled: false,
-              ),
-              const SizedBox(height: 8),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Telefone',
-                  hintText: '11978931234',
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 202, 200, 200),
-                ),
-                enabled: false,
-              ),
-              const SizedBox(height: 8),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  hintText: 'teste@gmail.com',
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 202, 200, 200),
-                ),
-                enabled: false,
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Ação do botão "Excluir conta"
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                    child: const Text('Excluir conta'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            width: 300,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Dados fornecedor',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              const EditarPerfilFornecedorScreen(),
+                ),
+                const SizedBox(height: 16),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nome',
+                    hintText: 'nome',
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 202, 200, 200),
+                  ),
+                  enabled: false,
+                ),
+                const SizedBox(height: 8),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'CNPJ',
+                    hintText: '1179891234',
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 202, 200, 200),
+                  ),
+                  enabled: false,
+                ),
+                const SizedBox(height: 8),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Telefone',
+                    hintText: '11978931234',
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 202, 200, 200),
+                  ),
+                  enabled: false,
+                ),
+                const SizedBox(height: 8),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    hintText: 'teste@gmail.com',
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 202, 200, 200),
+                  ),
+                  enabled: false,
+                ),
+                const SizedBox(height: 8),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    hintText: '********',
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 202, 200, 200),
+                  ),
+                  enabled: false,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Ação do botão "Excluir conta"
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
                         ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
                       ),
+                      child: const Text('Excluir conta'),
                     ),
-                    child: const Text('Editar dados'),
-                  ),
-                ],
-              ),
-            ],
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                const EditarPerfilFornecedorScreen(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: const Text('Editar dados'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
